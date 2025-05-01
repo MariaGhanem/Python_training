54c9d24370d083685863a009d371341aa6e0b87d
maria ghanem <ma.a.ghanem3@gmail.com>
2025-05-01 14:36:01 +0300

    Create Q2.py

---
+++ task2/Q2.py
@@ -0,0 +1,5 @@
+#Q2: Write a loop that prints the multiplication of a given number (Up to 10).
+num=int(input('pleas enter a number \n'))
+
+for i in range(1,11):
+    print(f'{i} X {num} ={i*num} ')
